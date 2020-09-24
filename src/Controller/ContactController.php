7fd8c3b6f32bfbd8425b8f68b6e3e39c4fd320a7
mahdi-hamdii <mahdi.hamdi@insat.u-carthage.tn>
2020-09-24 14:52:02 +0100

    24/09

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -14,20 +14,35 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact/{id}", name="contact")
      */
-    public function index(Request $request,Article $article)
+    public function index(Request $request,Article $article,\Swift_Mailer $mailer)
     {
         $categories=$this->getDoctrine()->getRepository(Categorie::class)->findAll();
-        $form=$this->createForm(ContactType::class);
+        $form=$this->createForm(ContactType::class,['action'=>$this->generateUrl('contact',['id'=>$article->getId()])]);
         $form->handleRequest($request);
         if ($form->isSubmitted()&&$form->isValid()){
-        $contact=$form->getData();
+            $contact=$form->getData();
+
         //ici nous enverrons le mail
-            dd($contact);
-        }
+            $message = ( new \Swift_Message('Demande Devis'))
+                ->setFrom($contact['email'])
+                ->setTo('mahdihamdi@live.fr')
+                ->setBody(
+                    $this->renderView(
+                        'contact/email.html.twig',['contact'=>$contact,'article'=>$article]
+                    ),
+                    'text'
+                );
+            //on  envoie le message
+            $mailer->send($message);
+            $this->addFlash('success','votre demande devis sera traité le plutot possible');
+            return $this->redirectToRoute('third_page',['id'=>$article->getId()]);
+        }else{
+
+
             return $this->render('contact/index.html.twig', [
                 'contactform' => $form->createView(),
                 'categories'=>$categories
             ]);
-
+        }
     }
 }
